d5c3c079fb20c7ac185ec6ff2abedbc0ed233962
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-01 10:34:54 +0200

    Added package.json and declared some string variable in ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    // only change code below this line
+    var myFirstName;
+    var myLastName;
+    var myHomeTown;
+    myFirstName = "Rusmira";
+    myLastName = "Sabic Mahic";
+    myHomeTown = "Bosanska Krupa";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
