26dd15c6cdf20feadeef3e6929230296f2eb8008
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-01 12:22:12 +0200

    Constructing strings with variables

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    var myName = "Rusmira";
+    var myString = "My name is " + myName + " and I am well!";
+    // Only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
